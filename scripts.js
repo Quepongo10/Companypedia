1d2d2ac3a05449fbe2a565f4786b77f1c165723d
Quepongo10 <101guilles@gmail.com>
2025-10-18 18:21:02 +0200

    Animacion chupipistachi del inicio

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-gsap.to("#telon", { 
-    duration: 2,
-    height: "50%",
-    ease: "power2.inOut"
-    
-});
+let tl = gsap.timeline();
+
+tl.to("#telon", { duration: 2, height: "50%", ease: "power2.inOut", delay: 2 })
+  .to("#telon", { duration: 2, height: "0%", ease: "power2.inOut", delay: 0.5 })
+  .to("#titulo_load", { duration: 0.5, opacity: 0 } , "-=1.5")
+  .to("#loading-screen", { duration: 1, opacity: 0, display: "none", ease: "power2.inOut" }, "-=1");
