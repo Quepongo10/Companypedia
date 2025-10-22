dd134174f103369bc9044f336f0e2345b2de71a4
Quepongo10 <101guilles@gmail.com>
2025-10-22 12:34:16 +0200

    cambio a la velocidad de la intro

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 let tl = gsap.timeline();
+let x = 1;
 
 // Animación de apertura del telón y desaparición del loading screen
-tl.to("#telon", { duration: 2, height: "50%", ease: "power2.inOut", delay: 1.5 })
-  .to("#telon", { duration: 2, height: "0.25%", ease: "power2.inOut", delay: 0.5 })
-  .to("#suelo", { duration: 2, opacity: 0, ease: "power2.inOut", delay: 0 })
-  .to("#telon", { duration: 1, top: "0%", ease: "power2.inOut" }, "-=1.5")
-  .to("#suelo", { duration: 1, top: "100%", ease: "power2.inOut" }, "-=1.5")
-  .to("#titulo_load", { duration: 0.5, opacity: 0 } , "-=3.5")
-  .to("#loading-screen", { duration: 1, opacity: 0, display: "none", ease: "power2.inOut" }, "-=1");
+tl.to("#telon", { duration: 1*x, height: "50%", ease: "power2.inOut", delay: 0.75*x })
+  .to("#telon", { duration: 1*x, height: "0.25%", ease: "power2.inOut", delay: 0.25*x })
+  .to("#suelo", { duration: 1*x, opacity: 0, ease: "power2.inOut", delay: 0 })
+  .to("#telon", { duration: 0.5*x, top: "0%", ease: "power2.inOut" }, "-=0.75'*x'")
+  .to("#suelo", { duration: 0.5*x, top: "100%", ease: "power2.inOut" }, "-=0.75'*x'")
+  .to("#titulo_load", { duration: 0.25*x, opacity: 0 } , "-=1.75'*x'")
+  .to("#loading-screen", { duration: 0.5*x, opacity: 0, display: "none", ease: "power2.inOut" }, "-=0.5'*x'");
 
 // Animación parpadeante del título principal
 gsap.to("#titulo_ani", {
